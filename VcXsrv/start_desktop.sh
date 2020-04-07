c43574272b193f63d282f939951fb53d3d965ddc
George Famelis <famelis@gmail.com>
2020-04-07 04:06:22 +0300

    Use the name server in order to find the host IP

--- VcXsrv/start_desktop.sh
+++ VcXsrv/start_desktop.sh
@@ -2,7 +2,11 @@
 XSESS="/home/$USER/.xsession.errors"
 (
 	export LIBGL_ALWAYS_INDIRECT=1
-	export DISPLAY=$(ipconfig.exe | grep IPv4 | cut -d: -f2 | sed -n -e '/^ 172/d' -e 's/ \([0-9\.]*\).*/\1:0.0/p')
+	export RX='[1-9][0-9]*\.[1-9][0-9]*\.[1-9][0-9]*\.[1-9][0-9]*'
+	export RS=$(sed -n -e 's/nameserver \('$RX'\).*/\1/p' /etc/resolv.conf)
+	export HS=$(cat /etc/hostname)
+	export DN="0.0"
+	export DISPLAY=$(dig $HS | sed -e '/^[;]/d' -e '/^$/d' -e '/'$RS'/d' -e 's/.*\t\('$RX'\).*/\1:'$DN'/')
 	echo DISPLAY is $DISPLAY
 
 	sudo su -c "service dbus start"
